83616ca85918890475bcf9ac6ce0b9022c1195cc
Denis <denis.semakin@3mdeb.com>
2022-02-25 16:41:09 +0300

    attester: remove EK hash from meta_data structure
    Signed-off-by: Denis <denis.semakin@3mdeb.com>

--- include/fobnail-attester/meta.h
+++ include/fobnail-attester/meta.h
@@ -3,31 +3,13 @@
 
 #include <stdint.h>
 
-/* Enum for hash id */
-enum {
-    SHA1,
-    SHA256,
-    SHA512
-};
-
-/* Structure for EK cert hash */
-struct EKcert_hash {
-    uint8_t id; /* specify what hash algo is currently used, could be equal SHA1, SHA256... */
-    union {
-        uint8_t sha1_hash[20];
-        uint8_t sha256_hash[32];
-        uint8_t sha512_hash[64];
-    } hash;
-};
-
 /* Structure for meta data for provisioning and attestation */
 struct meta_data {
-    uint8_t              header_version;
-    uint8_t              mac[6];         /* MAC address */
-    char           *serial_number;       /* Serial number */
-    char           *manufacturer;
-    char           *product_name;
-    struct EKcert_hash  EK_hash;  /* EKcert hash */
+    uint8_t     header_version;
+    uint8_t     mac[6];         /* MAC address */
+    char        *serial_number;       /* Serial number */
+    char        *manufacturer;
+    char        *product_name;
 } __attribute__((packed));
 
 #define META_H_VERSION  1
