b477f06785b7bc916f6148750d27b78c117b569e
Artur Kowalski <artur.kowalski@3mdeb.com>
2022-03-14 15:37:30 +0100

    Fix device path parsing.
    Fix path parsing to avoid crash when encountering physical and non-pci
    network adapters.

--- src/meta_data.c
+++ src/meta_data.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
@@ -43,13 +44,16 @@ static int obtain_mac_from_iface(const char *iface, uint8_t *dst_mac)
 /**
  * Parse the path and obtain the PCI BDF from it
  */
-static uint32_t get_bdf_from_path(const char *link_path)
+static int get_bdf_from_path(const char *link_path, uint32_t *out_bdf)
 {
     char *bdf;
     size_t bdf_str_len;
     char *colon1, *colon2, *dot, *slash1, *slash2, *null;
     uint32_t ret = 0UL;
     int x,y,z;
+
+    if (strncmp(link_path, "/sys/devices/pci", sizeof("/sys/devices/pci") - 1) != 0)
+        return -1;
 
     colon1 = strrchr(link_path, ':');
     slash1 = colon1;
@@ -81,7 +85,8 @@ static uint32_t get_bdf_from_path(const char *link_path)
     ret |= x << 16;
 
     free(bdf);
-    return ret;
+    *out_bdf = ret;
+    return 0;
 }
 
 static int get_mac_addr(uint8_t *mac)
@@ -134,10 +139,25 @@ static int get_mac_addr(uint8_t *mac)
         if (strstr(link_buf, "virtual"))
             continue;
 
-        curr_bdf = get_bdf_from_path(link_buf);
-        if (bdf.bdf_num == 0UL || bdf.bdf_num > curr_bdf) {
-            bdf.bdf_num = curr_bdf;
-            snprintf(bdf.iface, 32, "%s", (strrchr(link_buf, '/') + 1));
+        /* Convert path to absolute
+         * Path is constructed by concatenating SYS_CLASS_NET_PATH with relative
+         * path from readlink() so we need to allocate twice as much memory.
+         */
+        char relative_path[PATH_MAX*2];
+        snprintf(relative_path, PATH_MAX *2, "%s/%s", SYS_CLASS_NET_PATH, link_buf);
+        
+        char canonical_path[PATH_MAX];
+        if (!realpath(relative_path, canonical_path)) {
+            fprintf(stderr, "Failed to resolve canonical path of \"%s\": %s\n", dir->d_name, strerror(errno));
+            continue;
+        }
+
+        /* Ignore all network cards which are not connected to PCI */
+        if (get_bdf_from_path(canonical_path, &curr_bdf) >= 0) {
+            if (bdf.bdf_num == 0UL || bdf.bdf_num > curr_bdf) {
+                bdf.bdf_num = curr_bdf;
+                snprintf(bdf.iface, 32, "%s", (strrchr(link_buf, '/') + 1));
+            }
         }
     }
 
